747cbb3489511dfdd707acd16ebeef64df131c88
Giridhar Talla <giridhar.talla2002@gmail.com>
2024-01-04 10:07:45 +0530

    optimize

--- leetcode/2870-min-steps-to-empty-array.cpp
+++ leetcode/2870-min-steps-to-empty-array.cpp
@@ -3,43 +3,17 @@ public:
     int minOperations(vector<int>& nums) {
         unordered_map<int, int> mp;
         for (int i : nums) {
-            mp[i] += 1;
+            mp[i]++;
         }
 
-        int minWays = 0;
-        for (const auto& it : mp) {
+        int cnt = 0;
+        for (auto it : mp) {
             int n = it.second;
-            if (n < 2) {
-                return -1;
-            }
-
-            int cond = n % 3;
-            if (cond == 0) {
-                minWays += n / 3;
-                continue;
-            } else if (cond == 1) {
-                int threes = n / 3;
-                n -= (threes * 3);
-
-                if (n % 2 != 0) {
-                    threes--;
-                    n += 3;
-                }
-
-                int twos = n / 2;
-
-                minWays += threes + twos;
-                continue;
-            }
-
-            else if (cond == 2) {
-                minWays += (n / 3) + 1;               
-                continue;
-            } else {
-                return -1;
-            }
+            if(n == 1) return -1;
+            cnt += n/3;
+            if(n % 3) cnt++;
         }
 
-        return minWays;
+        return cnt;
     }
 };
